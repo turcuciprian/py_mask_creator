c53986a7510e3dedbcac8cb246c9961117990dda
turcuciprian <turcuciprian1@gmail.com>
2023-03-20 21:48:16 +0200

    first push, fully working (with the right folders)

---
+++ greyscale_generator.py
@@ -0,0 +1,25 @@
+from PIL import Image
+import os
+
+# Set the input and output folder paths
+input_folder = './beforeGreyscale'
+output_folder = './afterGreyscale'
+
+# Iterate through each file in the input folder
+for filename in os.listdir(input_folder):
+    # Check if the file is an image
+    if filename.endswith('.jpg') or filename.endswith('.png'):
+        # Open the image
+        image_path = os.path.join(input_folder, filename)
+        image = Image.open(image_path)
+        
+        # # Convert the image to greyscale with image mode rgb  
+        # Convert the image to black and white
+        image = image.convert('L')
+        
+        # Convert the image back to RGB mode
+        image = image.convert('RGB')
+        
+        # Save the black and white image in the output folder
+        output_path = os.path.join(output_folder, filename)
+        image.save(output_path)
---
+++ mask_generator.py
@@ -0,0 +1,37 @@
+from PIL import Image
+import numpy as np
+import os
+
+# Set the input and output folder paths
+input_folder = 'beforeMasks'
+output_folder = 'afterMasks'
+
+# Define the color threshold (in RGB)
+red_threshold = (255, 0, 0)
+color_threshold = 50
+
+# Iterate through each file in the input folder
+for filename in os.listdir(input_folder):
+    # Check if the file is an image
+    if filename.endswith('.jpg') or filename.endswith('.png'):
+        # Open the image
+        image_path = os.path.join(input_folder, filename)
+        image = Image.open(image_path)
+        
+        # Get the pixel data
+        pixel_data = image.load()
+        
+        # Iterate through each pixel in the image
+        for i in range(image.size[0]):
+            for j in range(image.size[1]):
+                # Check if the pixel is approximately red
+                if pixel_data[i, j][0] >= red_threshold[0] - color_threshold and pixel_data[i, j][1] <= red_threshold[1] + color_threshold and pixel_data[i, j][2] <= red_threshold[2] + color_threshold:
+                    # Replace approximately red pixels with white
+                    pixel_data[i, j] = (255, 255, 255)
+                else:
+                    # Replace other colored pixels with white
+                    pixel_data[i, j] = (0, 0, 0)
+        
+        # Save the new image in the output folder
+        output_path = os.path.join(output_folder, filename)
+        image.save(output_path, quality=100, optimize=False)
